742f28f3800ca583048fae888371f2a5a88be80b
alesleoingis <alesleoingis@gmail.com>
2013-09-22 11:06:48 +0400

    Initial Commit

---
+++ TestGitMongo/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  TestGitMongo
+//
+//  Created by  Vlad on 9/22/13.
+//  Copyright (c) 2013 Vlad. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
